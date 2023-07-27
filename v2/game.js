4e37693deb7b5776fa46efefd3ea53b491d784f0
Classroom <classroom@yeditepe.idr>
2023-07-27 10:42:58 +0300

    level finished popup

--- v2/game.js
+++ v2/game.js
@@ -4,6 +4,7 @@ import { startCommandInputListener } from "./modules/controller/commandInputs.js
 import { startKeyListener } from "./modules/controller/controller.js";
 import { makeConfettiEffect } from "./modules/gui/confetti/confetti.js";
 import { drawGame } from "./modules/gui/gui.js";
+import { popupLevelFinished } from "./modules/gui/gui2d/levelFinishedModal.js";
 import { getLevel1Board } from "./modules/levels/level1.js";
 
 //Initialize the canvas
@@ -25,9 +26,7 @@ export let gameConfig = {
 		this.context = this.canvas.getContext("2d");
 		this.ctx = this.context;
 		document.getElementById("gamecanvas").appendChild(this.canvas);
-
-		makeConfettiEffect(3000)
-
+		popupLevelFinished()
 		this.board = getLevel1Board()
 
 		startKeyListener();
--- v2/modules/game_logic/game_logic.js
+++ v2/modules/game_logic/game_logic.js
@@ -1,4 +1,6 @@
 import { gameConfig } from "../../game.js";
+import { makeConfettiEffect } from "../gui/confetti/confetti.js";
+import { popupLevelFinished } from "../gui/gui2d/levelFinishedModal.js";
 import { playBlockedSound, playCollectSound } from "../sound/player.js";
 
 
@@ -7,8 +9,9 @@ export function arrowArrivedAtGoalTile(tile) {
 	gameConfig.board.collectGoal(tile.row, tile.col);
 	playCollectSound();
     if (areAllGoalsEaten()){
-        makeConfettiEffect(1000)
-    }
+        makeConfettiEffect(3000)
+        popupLevelFinished()
+    }   
 
 }
 
